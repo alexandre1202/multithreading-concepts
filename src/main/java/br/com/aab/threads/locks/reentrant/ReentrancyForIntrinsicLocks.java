159eb48943a1e4a91bacf73105e32595e4600ef1
Alexandre A Barbosa <alexandre1202@gmail.com>
2024-01-20 19:05:22 -0300

    feat: practicing reentrant lock

--- src/main/java/br/com/aab/threads/locks/reentrant/ReentrancyForIntrinsicLocks.java
+++ src/main/java/br/com/aab/threads/locks/reentrant/ReentrancyForIntrinsicLocks.java
@@ -1,2 +1,19 @@
-package br.com.aab.threads.locks.reentrant;public class ReentrancyForIntrinsicLocks {
+package br.com.aab.threads.locks.reentrant;
+
+public class ReentrancyForIntrinsicLocks {
+    public synchronized void doSomething() {
+        System.out.printf("Running Parent doSomething ... ");
+    }
+}
+
+class SubReentrancyForIntrinsicLocks extends ReentrancyForIntrinsicLocks{
+    public synchronized void doSomething() {
+        System.out.printf("Calling doSomething()%n");
+        super.doSomething();
+    }
+
+    public static void main(String[] args) {
+        SubReentrancyForIntrinsicLocks sub = new SubReentrancyForIntrinsicLocks();
+        sub.doSomething();
+    }
 }
