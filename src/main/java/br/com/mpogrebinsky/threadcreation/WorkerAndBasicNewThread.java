99ef7406130603bb0e275a1f9ec2a85bf848d4d1
Alexandre A Barbosa <alexandre1202@gmail.com>
2024-03-03 16:06:54 -0300

    feat: new java concurrency multithreading course

---
+++ src/main/java/br/com/mpogrebinsky/threadcreation/WorkerAndBasicNewThread.java
@@ -0,0 +1,14 @@
+package br.com.mpogrebinsky.threadcreation;
+
+public class WorkerAndBasicNewThread {
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(() ->
+                System.out.printf("new Thread called [%s] ...%n", Thread.currentThread().getName()));
+
+        System.out.printf("This is %s thread before to start a new Thread %n", Thread.currentThread().getName());
+        thread.setName("New Child thread");
+        thread.start();
+        System.out.printf("This is %s thread after to start a new Thread %n", Thread.currentThread().getName());
+        Thread.sleep(1000);
+    }
+}
